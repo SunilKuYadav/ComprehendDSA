4012c2102ce6671be5e9e968126d4f336b0da939
Sunil Kumar Yadav <123sunilkr@gmail.com>
2022-12-07 12:32:24 +0530

    Update: color

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,8 @@
 const wait = (timer: number) => new Promise((res) => setTimeout(res, timer));
-const active = "red",
-  done = "green",
-  selected = "red",
+const active = "#0000ff",
+  done = "#00ff00",
+  selected = "#FFFF99",
   inactive = "lightgreen",
-  swapped = "#FFFF99";
+  swapped = "#ff0000";
 
 export { wait, active, done, inactive, selected, swapped };
