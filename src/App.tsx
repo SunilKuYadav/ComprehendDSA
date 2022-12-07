5e9a0f60f74d196f20f616d33517228503daa654
Sunil Kumar Yadav <123sunilkr@gmail.com>
2022-12-07 12:32:02 +0530

    Update: add merge sort

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Bar from "./components/Bar";
 import { ArrayBarProps } from "./_types";
-import { bubbleSort, inactive } from "./utils";
+import {
+  active,
+  bubbleSort,
+  done,
+  inactive,
+  mergeSort,
+  selected,
+  swapped,
+} from "./utils";
 
 var timer = 500;
 
 function App() {
   const [count, setCount] = useState(20);
   const [dataSet, setdataSet] = useState<ArrayBarProps[]>([]);
+  const [btnState, setBtnState] = useState(false);
 
   const handleSizeChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     console.log(e.target.value);
@@ -19,15 +28,13 @@ function App() {
   const updateData = () => {
     // update timer
     if (count >= 80) {
-      timer = 10;
+      timer = 30;
     } else if (count >= 50) {
       timer = 50;
     } else if (count >= 20) {
       timer = 100;
-    } else if (count >= 10) {
-      timer = 300;
     } else {
-      timer = 500;
+      timer = 300;
     }
     // create data set
     const tempDataSet: any[] = [];
@@ -37,15 +44,28 @@ function App() {
       data = Math.floor(Math.random() * (100 - 5 + 1)) + 5;
     }
     setdataSet(() => [...tempDataSet]);
+    console.log(tempDataSet.map((item) => item.number));
   };
 
   const updateUI = (data: ArrayBarProps[]) => {
     setdataSet(() => [...data]);
+    // console.log(data.map((item) => item.number));
     console.log(data);
   };
 
-  const sorting = () => {
-    bubbleSort(dataSet, timer, updateUI);
+  const sorting = async (type: string) => {
+    setBtnState(true);
+    switch (type) {
+      case "bubble":
+        await bubbleSort(dataSet, timer, updateUI);
+        break;
+      case "merge":
+        await mergeSort(dataSet, 0, dataSet.length - 1, timer, updateUI);
+        break;
+      default:
+        break;
+    }
+    setBtnState(false);
   };
 
   useEffect(updateData, [count]);
@@ -53,6 +73,7 @@ function App() {
   return (
     <div className="App">
       <input
+        disabled={btnState}
         type="range"
         min={5}
         max={100}
@@ -76,8 +97,30 @@ function App() {
             />
           ))}
       </div>
-      <button onClick={updateData}>generate</button>
-      <button onClick={sorting}> sort</button>
+      <button disabled={btnState} onClick={updateData}>
+        generate
+      </button>
+      <button disabled={btnState} onClick={() => sorting("bubble")}>
+        {" "}
+        bubble sort
+      </button>
+      <button disabled={btnState} onClick={() => sorting("merge")}>
+        {" "}
+        merge sort
+      </button>
+      <div>
+        <input type="color" disabled value={active} />
+        <span>Extra Space</span>
+
+        <input type="color" disabled value={selected} />
+        <span>Comparing</span>
+
+        <input type="color" disabled value={swapped} />
+        <span>Swapping</span>
+
+        <input type="color" disabled value={done} />
+        <span>Done Sorting</span>
+      </div>
     </div>
   );
 }
