a7f8f5e2771716863561b380cae1b76bf5ab22a4
Sunil Kumar Yadav <123sunilkr@gmail.com>
2022-12-08 09:57:56 +0530

    Add: insertion sort file

---
+++ src/utils/sorting/insertionSorting.ts
@@ -0,0 +1,38 @@
+import { ArrayBarProps } from "../../_types";
+import { done, swapped, wait } from "../utils";
+
+export const insertionSort = async (
+  arr: ArrayBarProps[],
+  timer: number,
+  stepData: (data: ArrayBarProps[]) => void
+) => {
+  const len = arr.length;
+  let i, j, key;
+  for (i = 0; i < len; i++) {
+    key = arr[i];
+    j = i - 1;
+
+    while (j >= 0 && arr[j].number > key.number) {
+      let a = arr[j + 1].color;
+      let b = arr[j].color;
+      arr[j + 1] = { ...arr[j + 1], color: swapped };
+      arr[j] = { ...arr[j], color: swapped };
+      stepData(arr);
+      await wait(timer);
+
+      arr[j + 1] = arr[j];
+
+      arr[j] = { ...arr[j], color: b };
+      arr[j + 1] = { ...arr[j + 1], color: a };
+      stepData(arr);
+      await wait(timer);
+
+      j -= 1;
+    }
+    arr[j + 1] = key;
+    arr[j + 1] = { ...arr[j + 1], color: done };
+    stepData(arr);
+    await wait(timer);
+  }
+  stepData(arr);
+};
