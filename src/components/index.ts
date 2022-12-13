9d63edeecbab92c76d7ca96a7a7d41a3a3a725bb
Sunil Kumar Yadav <123sunilkr@gmail.com>
2022-12-13 12:06:18 +0530

    Update: export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+export { default as ArrayNodeBar } from "./arrays/ArrayNodeBar";
+export { default as ArrayNodeBox } from "./arrays/ArrayNodeBox";
+export { default as Footer } from "./footer";
+export { default as Header } from "./header";
 export * from "./modal";
-export { default as ArrayNodeBar } from "./ArrayNodeBar";
-export { default as ArrayNodeBox } from "./ArrayNodeBox";
