295b8ad768cc040928c5c60f792ffad3019e44d1
Sunil Kumar Yadav <123sunilkr@gmail.com>
2022-12-17 10:58:19 +0530

    Add: sorting

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -19,7 +19,7 @@ const CARD_DATA = [
   },
   {
     name: "Matrix/Grid",
-    operation: ["create"],
+    operation: ["create", "sort"],
     link: "/matrix",
   },
   {
@@ -39,6 +39,11 @@ const CARD_DATA = [
   },
   {
     name: "Graph",
+    operation: [],
+    link: "",
+  },
+  {
+    name: "Binary Tree",
     operation: [],
     link: "",
   },
